3947767b6348c3d73643317ccee8b706501809c3
Rostyslav Mydliak <rim1972ua@gmail.com>
2022-09-01 15:15:32 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector(".change-color");
+
+buttonChangeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.querySelector(".color").textContent = randomColor;
+  document.querySelector("body").style.backgroundColor = randomColor;
+});
